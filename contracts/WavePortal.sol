297fb11b09a3da4522b7a405986682c54eb45009
Aaron Janes <aaron.janes@gmail.com>
2021-11-06 17:38:11 -0600

    Looping in solidity

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 contract WavePortal {
   uint256 totalWaves;
-
+  address[] listOfSenders;
 
   constructor() {
     console.log("Yo yo, I am a contract and I'm smart ");
@@ -14,10 +14,18 @@ contract WavePortal {
   function wave() public {
     totalWaves += 1;
     console.log("%s has waved!", msg.sender);
+    listOfSenders.push(msg.sender);
   }
 
   function getTotalWaves() public view returns (uint256) {
     console.log("We have %d total waves!", totalWaves);
+
+    for(uint i; i < listOfSenders.length; i++){
+      console.log("Log of all wavers %d  %d",i, listOfSenders[i] );
+    }
     return totalWaves;
   }
+
+
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -9,12 +9,19 @@ const main = async () => {
   let waveCount
   waveCount = await waveContract.getTotalWaves()
 
-  const waveTxn = await waveContract.wave()
+  let waveTxn = await waveContract.wave()
   await waveTxn.wait()
+
   waveCount = await waveContract.getTotalWaves()
 
   waveCount = await waveContract.wave()
   await waveTxn.wait()
+
+  waveCount = await waveContract.getTotalWaves()
+
+  waveTxn = await waveContract.connect(randomPerson).wave()
+  await waveTxn.wait()
+
   waveCount = await waveContract.getTotalWaves()
 }
 
