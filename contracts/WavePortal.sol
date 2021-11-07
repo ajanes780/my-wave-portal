458ee8281ee47aa40b7f0d9d75112f8f622f569b
Aaron Janes <aaron.janes@gmail.com>
2021-11-07 09:00:29 -0700

    test deploy

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,9 +6,9 @@ import "hardhat/console.sol";
 contract WavePortal {
   uint256 totalWaves;
   address[] listOfSenders;
-
+  uint256 private seed;
 //  --- will they listen --- //
-  event NewWave(address indexed from, uint256 timestamp, string message);
+  event NewWave(address indexed from, uint256 timestamp, string message, string userName);
 
 
 
@@ -25,26 +25,51 @@ contract WavePortal {
 
   constructor() payable {
     console.log("Yo yo, I am a contract and I'm smart");
+    seed = (block.timestamp + block.difficulty) % 100;
   }
 
+  mapping(address => uint256) public lastWavedAt;
+
   function wave(string memory _message,  string memory _userName) public {
+    // require(
+    //           lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+    //           "Wait 15m"
+    //       );
+
+    require(lastWavedAt[msg.sender] + 30 seconds < block.timestamp, "Must wait 30 seconds before waving again.");
+
+    lastWavedAt[msg.sender] = block.timestamp;
+
     totalWaves += 1;
     console.log("%s has waved!", msg.sender);
     listOfSenders.push(msg.sender);
+    seed = (block.difficulty + block.timestamp + seed) % 100;
+
+
 
 
   // --- create array of struts --//
   waves.push(Wave(msg.sender, _message, _userName, block.timestamp));
 
+
+
+
+      if (seed <= 50) {
+        console.log("%s won!", _userName);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "contract is broke ass no prize for you."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
+    }
+
+  emit NewWave(msg.sender, block.timestamp, _message, _userName);
+ }
   //  --- spread the news --//
-  emit NewWave(msg.sender, block.timestamp, _message);
 
-  uint256 prizeAmount = 0.0001 ether;
-  require(prizeAmount <= address(this).balance,"Yo, this contract can't even pay you out!");
-
-  (bool success, )=(msg.sender).call{value: prizeAmount}("");
-  require(success, "Failed to withdraw money from contract ");
-  }
 
 
 function getAllWaves() public view returns (Wave[] memory) {
@@ -59,4 +84,4 @@ function getAllWaves() public view returns (Wave[] memory) {
 
 
 
-}
+  }
--- scripts/run.js
+++ scripts/run.js
@@ -1,43 +1,28 @@
 const main = async () => {
- 
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
   const waveContract = await waveContractFactory.deploy({
     value: hre.ethers.utils.parseEther('0.1'),
   })
   await waveContract.deployed()
-  console.log('Contract deployed to:', waveContract.address)
-
-  // --- check contract balance -- //
+  console.log('Contract addy:', waveContract.address)
 
   let contractBalance = await hre.ethers.provider.getBalance(waveContract.address)
   console.log('Contract balance:', hre.ethers.utils.formatEther(contractBalance))
 
-  let waveCount
-  waveCount = await waveContract.getTotalWaves()
-  console.log(waveCount.toNumber())
-
-  //  --- Send Waves ---//
-  let waveTxn = await waveContract.wave('Hi Aaron', 'Monica')
-  await waveTxn.wait() // wait on the miners
-  waveCount = await waveContract.getTotalWaves()
-  console.log(waveCount.toNumber())
-
   /*
-   * Get Contract balance to see what happened!
+   * Let's try two waves now
    */
+  const waveTxn = await waveContract.wave('This is wave #1', 'chandler')
+  await waveTxn.wait()
+
+  const waveTxn2 = await waveContract.wave('This is wave #2', 'Joey')
+  await waveTxn2.wait()
+
   contractBalance = await hre.ethers.provider.getBalance(waveContract.address)
   console.log('Contract balance:', hre.ethers.utils.formatEther(contractBalance))
 
-  
-
-  const [_, randomPerson] = await hre.ethers.getSigners()
-  waveTxn = await waveContract.connect(randomPerson).wave(' Was up from the blockchain', 'Chandler')
-  await waveTxn.wait()
-
   let allWaves = await waveContract.getAllWaves()
   console.log(allWaves)
-
-  
 }
 
 const runMain = async () => {
